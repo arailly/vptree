496f2117e51c5e6e9ddabd6da720bf67355a2930
arailly <yusuke.arailly@gmail.com>
2019-12-12 10:52:40 +0900

    :+1: update distance

--- main.cpp
+++ main.cpp
@@ -24,11 +24,13 @@ int main() {
     const string query_path = config["query_path"];
     const string save_path = config["save_path"];
     const unsigned n = config["n"];
+    const unsigned n_query = config["n_query"];
     const float range = config["range"];
+    const string& distance = config["distance"];
 
-    const auto queries = read_csv(query_path);
+    const auto queries = read_csv(query_path, n_query);
 
-    auto vpt = VPTree();
+    auto vpt = VPTree(distance);
     vpt.build(data_path, n);
     cout << "complete: build" << endl;
 
